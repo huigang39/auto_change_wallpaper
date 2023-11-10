3003fa245a7bf348ac7badc0ccf7a7c92affd9ca
huigang39 <huigang39@outlook.com>
2023-11-10 19:07:48 +0800

    cpp与hpp代码分离

--- include/getImage.hpp
+++ include/getImage.hpp
@@ -26,42 +26,4 @@ public:
     ~getImage();
 };
 
-getImage::getImage(const std::string &url, const std::string &filename)
-{
-    CURL *curl;
-    FILE *fp;
-    CURLcode res;
-    curl = curl_easy_init();
-    if (curl)
-    {
-        fp = fopen(filename.c_str(), "wb");
-        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
-        res = curl_easy_perform(curl);
-
-        // 检查错误
-        if (res != CURLE_OK)
-        {
-            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-        }
-
-        // 清理curl
-        curl_easy_cleanup(curl);
-        fclose(fp);
-    }
-}
-
-getImage::~getImage()
-{
-}
-
-size_t getImage::WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
-{
-    size_t realsize = size * nmemb;
-    FILE *fp = (FILE *)userp;
-    fwrite(contents, size, nmemb, fp);
-    return realsize;
-}
-
 #endif
